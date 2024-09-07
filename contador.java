0c85c0dc46d4c81948ba3d4d951cc29035ba49a5
JDiazRzo <159645527+JDiazRzo@users.noreply.github.com>
2024-09-07 13:44:23 -0500

    Add files via upload

---
+++ contador.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class contador {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String userphrase;
+        userphrase = scan.nextLine().toLowerCase();
+        int countA = 0, countE = 0, countI = 0, countO = 0, countU = 0;
+
+        for (int i=0; i <userphrase.length(); i ++) {
+            char c = userphrase.charAt(i);
+            switch (c) {
+                case 'a':
+                    countA++;
+                    break;
+                case 'e':
+                    countE++;
+                    break;
+                case 'i':
+                    countI++;
+                    break;
+                case 'o':
+                    countO++;
+                    break;
+                case 'u':
+                    countU++;
+                    break;
+        
+            }
+        }
+       
+        System.out.println(countA + "," + countE + "," + countI + "," + countO + "," + countU + " ");
+        scan.close();
+    }
+    
+}
